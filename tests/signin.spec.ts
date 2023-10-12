26fb6f7e4efb3b42b1d97bcf38b64e1c58f162be
Pipeline <pipeline@demo.com>
2023-10-12 11:48:54 -0500

    Update playwright templates

--- tests/signin.spec.ts
+++ tests/signin.spec.ts
@@ -4,13 +4,16 @@ export default defineConfig({
   reporter: ['junit', { outputFile: 'test-results/e2e-junit-results.xml' }],
 });
 
+// BASE_URL = 'https://lll-playwright-web.azurewebsites.net/'
+// USERNAME = 'TestUser@lyleluppes.onmicrosoft.com'
+// PASSWORD = '8Y94!sXf'
 test('Login', async ({ page }) => {
-  await page.goto('https://lll-playwright-web.azurewebsites.net/');
+  await page.goto(process.env.BASE_URL);
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle('Sign in to your account');
-  await page.getByLabel('Enter your email, phone, or Skype.').fill('TestUser@lyleluppes.onmicrosoft.com');
+  await page.getByLabel('Enter your email, phone, or Skype.').fill(process.env.USERNAME);
   await page.getByRole('button', { name: 'Next' }).click();
-  await page.getByPlaceholder('Password').fill('8Y94!sXf');
+  await page.getByPlaceholder('Password').fill(process.env.PASSWORD);
   await page.getByRole('button', { name: 'Sign in' }).click();
   await page.getByRole('button', { name: 'Yes' }).click();
 });
